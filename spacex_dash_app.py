545a25fa37643136ddef6df175f1796764ffe817
Julia Sacca <168309075+juliasacca@users.noreply.github.com>
2025-11-08 15:50:35 -0500

    Fix color mapping syntax in spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -83,9 +83,9 @@ def get_pie_chart(entered_site):
                      color='Outcome',
                      color_discrete_map={
                         'CCAFS LC-40': '#945F56',
-                        'VAFB SLC-4E', '#f39685',
-                        'KSC LC-39A', '#f2dacf',
-                        'CCAFS SLC-40', '#D6BCB7'
+                        'VAFB SLC-4E': '#f39685',
+                        'KSC LC-39A': '#f2dacf',
+                        'CCAFS SLC-40': '#D6BCB7'
                       }
                     )
         return fig
@@ -113,13 +113,13 @@ def get_scatter_chart(entered_site, entered_payload):
                      x='Payload Mass (kg)',
                      y='class',
                      color='Booster Version Category',
-                     title="Correlation between Payload and Launch Success"
+                     title="Correlation between Payload and Launch Success",
                      color_discrete_map={
                         'v1.0': '#945F56',
-                        'v1.1', '#f39685',
-                        'FT', '#f2dacf',
-                        'B4', '#D6BCB7',
-                        'B5', '#c0504d'
+                        'v1.1': '#f39685',
+                        'FT': '#f2dacf',
+                        'B4': '#D6BCB7',
+                        'B5': '#c0504d'
                       }
                     )
     return fig
